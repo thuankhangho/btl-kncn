655884d0608e5c2bd059ff617e0ea2516a9e4866
VDTHP <136230459+VDT1805@users.noreply.github.com>
2023-10-29 20:51:25 +0700

    adding new UI lib bcz React is a wack and to bloat file size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,40 +61,88 @@ import { useDropzone } from 'react-dropzone';
 //   );
 // }
 
-function App() {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    acceptedFiles.forEach((file: File) => {
-      const reader = new FileReader()
+// function App() {
+//   const onDrop = useCallback((acceptedFiles: File[]) => {
+//     acceptedFiles.forEach((file: File) => {
+//       const reader = new FileReader()
 
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-      // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
-      }
-      reader.readAsText(file)
-    })
+//       reader.onabort = () => console.log('file reading was aborted')
+//       reader.onerror = () => console.log('file reading has failed')
+//       reader.onload = () => {
+//       // Do whatever you want with the file contents
+//         const binaryStr = reader.result
+//         console.log(binaryStr)
+//       }
+//       reader.readAsText(file)
+//     })
     
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+//   }, [])
+//   const {fileRejections,getRootProps, getInputProps} = useDropzone({accept: {'text/*': ['txt'],},onDrop})
 
+//   return (
+//     <div {...getRootProps()}
+//     style={{
+//               display: 'flex',
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               height: '50px',
+//               width: '300px',
+//               border: '1px dotted',
+//               backgroundColor:'lightgray' ,
+//             }}>
+//       <input {...getInputProps()} />
+//       <p>Drag 'n' drop some files here, or click to select files (.txt only)</p>
+//     </div>
+//   )
+// }
+
+import { Group, Text, rem } from '@mantine/core';
+import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
+import { Dropzone, DropzoneProps } from '@mantine/dropzone';
+import '@mantine/dropzone/styles.css';
+export function App(props: Partial<DropzoneProps>) {
   return (
-    <div {...getRootProps()}
-    style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '50px',
-              width: '300px',
-              border: '1px dotted',
-              backgroundColor:'lightgray' ,
-            }}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </div>
-  )
-}
+    <Dropzone
+      onDrop={(files) => {console.log('accepted files', files)}}
+      onReject={(files) => console.log('rejected files', files)}
+      maxFiles={1}
+      accept={
+        {'text/plain': ['.txt']}
+      }
+      {...props}
+      
+    >
+      <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+        <Dropzone.Accept>
+          <IconUpload
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <IconX
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Reject>
+        <Dropzone.Idle>
+          <IconPhoto
+            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+            stroke={1.5}
+          />
+        </Dropzone.Idle>
 
+        <div>
+          <Text size="xl" inline>
+            Quăng file hoặc nhắn vào đây để chọn
+          </Text>
+          <Text size="sm" c="dimmed" inline mt={7}>
+            Chỉ có thể upload 1 file .txt
+          </Text>
+        </div>
+      </Group>
+    </Dropzone>
+  );
+}
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme } from '@mantine/core';
+const theme = createTheme({
+  fontFamily: 'Montserrat, sans-serif',
+  defaultRadius: 'md',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
+
+
