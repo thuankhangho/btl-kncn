2a8fd17746dc68992518ca2562cef5c3f757dddb
thuankhangho <hothuankhang@gmail.com>
2023-11-09 15:42:24 +0700

    Thêm lật thẻ tự động

--- client/src/components/flashcard.tsx
+++ client/src/components/flashcard.tsx
@@ -1,27 +1,57 @@
 import { useLocation } from 'react-router-dom';
 import { FlashcardArray } from "react-quizlet-flashcard";
+import { useEffect, useRef, useState } from 'react';
 
 export default function Flashcard() {
   const location = useLocation();
   const data = location.state?.data;
-  const input = data.split('\n')
+  const input = data.split("\n");
 
-  const res = new Array<Object>;
-  var idx = 1;
-  for (var x of input) {
-    var temp = x.split(':');
-    res.push({
-      id: idx,
+  const res = input.map((element: String) => {
+    var temp = element.split(':');
+    return {
       frontHTML: temp[0],
       backHTML: temp[1]
-    })
-    idx++;
-  }
-  console.log(res);
+    }
+  })
+
+  const [autoflip, setAutoflip] = useState(false);
+  const currentCardFlipRef = useRef();
+  const controlRef = useRef({});
+
+  useEffect(() => {
+    if (autoflip == false)
+      return;
+    //Implementing the setInterval method 
+    currentCardFlipRef.current()
+    const interval = setInterval(async () => {
+        controlRef.current.nextCard()
+        await delay(100)
+        currentCardFlipRef.current()
+      }, 2000)
+
+    //Clearing the interval 
+    return () => clearInterval(interval); 
+}); 
+  
+
+  // var idx = 1;
+  // for (var x of input) {
+  //   var temp = x.split(':');
+  //   res.push({
+  //     id: idx,
+  //     frontHTML: temp[0],
+  //     backHTML: temp[1]
+  //   })
+  //   idx = idx + 1;
+  // }
+  // console.log(res);
 
   return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
     <div className="storyContainer">
       <FlashcardArray
+      // cycle={true}
         cards={res}
         frontContentStyle={{
           color: "black",
@@ -38,7 +68,21 @@ export default function Flashcard() {
         FlashcardArrayStyle={{
           color: "black"
         }}
+        currentCardFlipRef={
+          currentCardFlipRef
+        }
+        forwardRef={
+          controlRef
+        }
       />
     </div>
+    <button onClick={() => {
+      setAutoflip(!autoflip)
+      }}>Flip</button>
+    </div>
   );
+}
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
