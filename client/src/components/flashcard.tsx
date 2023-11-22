2071a52b86580f9890cad93bd44f1aef3b49f62a
thuankhangho <hothuankhang@gmail.com>
2023-11-22 17:08:24 +0700

    Thêm comment

--- client/src/components/flashcard.tsx
+++ client/src/components/flashcard.tsx
@@ -2,14 +2,14 @@ import { useLocation } from "react-router-dom";
 import { FlashcardArray } from "react-quizlet-flashcard";
 import { useEffect, useRef, useState } from "react";
 
-// Adjust the component function definition to include the new prop
-export default function Flashcard() {
-  const location = useLocation();
-  const data = location.state.data;
-  const input = data.split("\n");
-  const flashcardTextSize = location.state.textSize;
-  const size = input.length;
+const FlashcardApp: React.FC = () => {
+  const location = useLocation(); //lấy input từ đường link
+  const data = location.state.data; //lấy dữ liệu flashcard
+  const input = data.split("\n"); //tách chuỗi theo \n
+  const flashcardTextSize = location.state.textSize; //lấy cỡ chữ của flashcard
+  const size = input.length; //độ dài list flashcard
 
+  // tách từng flashcard thành mặt trước & mặt sau theo ：
   const res = input.map((element: string) => {
     var temp = element.split("：");
     return {
@@ -18,26 +18,24 @@ export default function Flashcard() {
     };
   });
 
-  const [autoflip, setAutoflip] = useState(false);
-  const [indexElement, setIndexElement] = useState(1);
-  const currentCardFlipRef = useRef();
-  const controlRef = useRef({});
+  const [autoflip, setAutoflip] = useState(false); // biến bool quản lý lật tự động
+  const [indexElement, setIndexElement] = useState(1); // biến number quản lý index của flashcard
+  const currentCardFlipRef = useRef(); // biến quản lý việc lật thẻ hiện tại
+  const controlRef = useRef({}); // biến quản lý việc lật thẻ
 
   useEffect(() => {
-    if (autoflip == false) return;
-
-    currentCardFlipRef.current();
+    if (autoflip == false) return; //nếu không bật autoflip -> thoát hàm
+    currentCardFlipRef.current(); //lật thẻ hiện tại
     const interval = setInterval(async () => {
-      controlRef.current.nextCard();
+      controlRef.current.nextCard(); //chuyển sang thẻ tiếp theo
       await delay(1000);
-      currentCardFlipRef.current();
+      currentCardFlipRef.current(); //lật thẻ hiện tại
       if (indexElement === size) {
-        controlRef.current.resetArray();
-        setIndexElement(1);
+        controlRef.current.resetArray(); //xóa array & sinh lại
+        setIndexElement(1); //trở về thẻ đầu tiên
       }
     }, 2000);
-
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); //xóa interval
   });
 
   return (
@@ -52,26 +50,26 @@ export default function Flashcard() {
       <div className="storyContainer">
         <FlashcardArray
           cards={res}
-          frontContentStyle={{
+          frontContentStyle={{ //mặt trước flashcard
             color: "black",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             fontSize: `${flashcardTextSize}rem`,
           }}
-          backContentStyle={{
+          backContentStyle={{ //mặt sau flashcard
             color: "black",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             fontSize: `${flashcardTextSize}rem`,
           }}
-          FlashcardArrayStyle={{
+          FlashcardArrayStyle={{ //style cho flashcard array
             color: "black",
             fontSize: `${flashcardTextSize}rem`,
           }}
-          currentCardFlipRef={autoflip ? currentCardFlipRef : undefined}
-          forwardRef={autoflip ? controlRef : undefined}
+          currentCardFlipRef={autoflip ? currentCardFlipRef : undefined} //điều khiển thẻ hiện tại
+          forwardRef={autoflip ? controlRef : undefined} //điều khiển di chuyển thẻ
           onCardChange={(_id, index) => {
             setIndexElement(index);
           }}
@@ -89,6 +87,8 @@ export default function Flashcard() {
   );
 }
 
-function delay(ms: number | undefined) {
+function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export default FlashcardApp;
